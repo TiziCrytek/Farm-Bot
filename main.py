c1e65e306a205717f380713b38df6cab6fe4a2e5
TiziCrytek <121620908+TiziCrytek@users.noreply.github.com>
2024-01-25 14:18:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ from time import sleep
 from farm_bot import FarmBot
 
 def main():
-    ip = input('IP and PORT:' )
-    bot = FarmBot(ip)
+    bot = FarmBot('5.tcp.eu.ngrok.io:12905')
     bot.start()
