c3232b5034d41116722ee1a38635e34bea19e7cb
TiziCrytek <121620908+TiziCrytek@users.noreply.github.com>
2024-01-25 14:30:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from time import sleep
 from farm_bot import FarmBot
 
 def main():
-    bot = FarmBot('5.tcp.eu.ngrok.io:12905')
     input('Enter to Start')
+    bot = FarmBot('5.tcp.eu.ngrok.io:12905')
     bot.start()
 
 main()
