8cae41735472de196e154488845b2ba684a93e53
TiziCrytek <121620908+TiziCrytek@users.noreply.github.com>
2024-01-26 20:49:10 +0200

    Add files via upload

--- farm_bot.py
+++ farm_bot.py
@@ -1,138 +1,153 @@
 import uiautomator2 as u2
-import json, random
+import json, random, subprocess
 from time import sleep
 
 class FarmBot:
     def __init__(self, addres):
-        self.connect_deviec(addres)
         self.alive = True
-
+        self.d = u2.connect(addres)
         self.home_btn = self.d(description="Главная")
         self.games_btn = self.d(description="Игры")
         self.bonus_btn = self.d(className='android.widget.Button', index=1, description='Забрать', enabled='true')
         self.quiz_btn = self.d(className='android.widget.Button', index=5, description='Забрать', enabled='true')
+        self.aim_game = self.d(className='android.widget.Button', index=6, description='Забрать', enabled='true')
+
+        self.back_btn = self.d(resourceId="com.android.systemui:id/back")
 
         self.package = 'com.skinsfarm.farm'
         self.activity = '.MainActivity'
 
+        self.pm_avtivity = 'com.google.android.finsky.activities.MainActivity'
+        self.ads_activity = 'com.applovin.adview.AppLovinFullscreenThemedActivity'
+
     def main(self):
-        while True:
+        while self.alive:
             if self.alive:
                 if not self.check_app():
                     self.app_start()
                     sleep(2)
                     self.games_btn.click()
-
-                if self.quiz_btn.click_exists():
-                    sleep(2)
-                    # self.quiz()
-                    self.d.app_stop(self.package)
-                    self.d.screen_off()
-                    sleep(60*60*3 + random.randint(1, 20))
-                    self.d.screen_on()
-                    self.d.swipe(500, 1000, 500, 500, 400)
-
+            
                 # if self.bonus_btn.click_exists():
                 #     self.get_bonus()
 
+                sleep(1.5)
+                if self.quiz_btn.click_exists():
+                    sleep(2)
+                    self.quiz()
+                    if self.alive:
+                        self.d.app_stop(self.package)
+                        self.d.screen_off()
+                        sleep(60*60*3 + random.randint(1, 20))
+                        self.d.screen_on()
+                        self.d.swipe(500, 1300, 500, 200, duration=0.1)
+                else:
+                    self.d.app_stop(self.package)
+                    sleep(2)
+                    self.d.screen_off()
+                    sleep(30*1 + random.randint(1, 10))
+                    self.d.screen_on()
+                    self.d.swipe(500, 1300, 500, 200, duration=0.1)
+
     def quiz(self):
         while self.alive:
-            if self.d(description="Далее").click_exists():
-                sleep(0.8)
-                if self.d(description="Закрыть").click_exists():
-                    return
+            try:
+                sleep(1.6)
+                if self.d(description="Далее").click_exists():
+                    sleep(0.8)
+                    if self.d(description="Закрыть").click_exists():
+                        return
+                    
+                with open('db.json', 'r', encoding='utf-8') as file:
+                    data = json.load(file)
 
-            with open('db.json', 'r', encoding='utf-8') as file:
-                data = json.load(file)
+                title = self.d(className='android.view.View', index='7').info.get('contentDescription')
+                btns = self.d(className='android.widget.Button')
 
-            sleep(3)
+                print(title)
 
-            title = self.d(className='android.view.View', index='7').info.get('contentDescription')
-            btns = self.d(className='android.widget.Button')
+                if title not in data:
+                    data[title] = {"q": "", "w": []}
 
-            if title not in data:
-                data[title] = {"q": "", "w": []}
+                n = 0
+                for btn in btns:
+                    if not self.alive:
+                        return
+                    n += 1
+                    btn_desc = btn.info['contentDescription']
 
-            buttons_list = list(btns)
-
-            random.shuffle(buttons_list)
-            bl = []
-            b = False
-            print()
-            print(title)
-
-            for btn in buttons_list:
-                if btn.info['contentDescription'] == data[title]['q']:
-                    b = False
-                    print(f'DB SUCCESS {btn.info["contentDescription"]}')
-                    btn.click()
-                    break
-                
-                if data[title]['w'] != [] and btn.info['contentDescription'] in data[title]['w']:
-                    print(f'SKIP: {btn.info["contentDescription"]}')
-                    continue
-
-                bl.append(btn)
-                b = True
-
-            if b:
-                for btn in bl:
-
-                    test_btn = btn.info['contentDescription']
-                    sleep(0.5)
-                    btn.click()
-                        
-                    sleep(2)
-
-                    if self.d(description="Выйти").exists:
-                        print(f'DARK: {test_btn}')
-                        data[title]["w"].append(test_btn)
-                        with open('db.json', 'w', encoding='utf-8') as file:
-                            json.dump(data, file, indent=4, ensure_ascii=False)
-                        self.d(description="Выйти").click()
-                        # d(description="Посмотреть рекламу").click()
-                        
-                        # while True:
-                        #     if d.xpath('//*[@resource-id="android:id/content"]/android.widget.FrameLayout[1]/android.view.View[1]/android.view.View[1]/android.view.View[1]/android.view.View[1]/android.widget.ImageView[2]').exists:
-                        #         print('OK')
-                        #         break
-
-                        #     # if d.xpath('//*[@resource-id="android:id/content"]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]').exists:
-                        #     #     d.xpath('//*[@resource-id="android:id/content"]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]').click()
-                            
-                        #     sleep(1)
-                        break
-                    else:
-                        print(f'SUCCESS: {test_btn}')
-                        data[title]["q"] = test_btn
-                        with open('db.json', 'w', encoding='utf-8') as file:
-                            json.dump(data, file, indent=4, ensure_ascii=False)
+                    if btn_desc == data[title]['q']:
+                        print(f'SUCCESS {btn_desc}')
+                        btn.click()
                         break
 
-                sleep(2)
-                self.quiz_btn.click()
+                    if n == 4:
+                        btn.click()
+                        sleep(2)
+                        if self.d(description="Выйти").click_exists():
+                            print(f'DARK: {btn_desc}')
+                            data[title]["w"].append(btn_desc)
+                            with open('db.json', 'w', encoding='utf-8') as file:
+                                json.dump(data, file, indent=4, ensure_ascii=False)
+                            self.quiz_btn.click()
+                            break
+                        else:
+                            print(f'DB: {btn_desc}')
+                            data[title]["q"] = btn_desc
+                            with open('db.json', 'w', encoding='utf-8') as file:
+                                json.dump(data, file, indent=4, ensure_ascii=False)
+                            break
+
+                    if data[title]['w'] != [] and btn_desc in data[title]['w']:
+                        print(f'SKIP: {btn_desc}')
+                        continue
+            except:
+                return
 
     def get_bonus(self):
-        sleep(1)
-        self.d(description="Забрать бонус").click_exists()
-        sleep(31)
+        sleep(2)
+        if self.d(description="Забрать бонус").click_exists():
+            while self.alive:
+                if not self.check_ads():
+                    continue
+
+                sleep(45)
+
+                if self.d.xpath('//*[@resource-id="android:id/content"]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]').click_exists():
+                    break
+                
+                self.d.click()
+
+                sleep(10)
+
+                sleep(10)
+        else:
+            self.games_btn.click()
 
     def start(self):
-        self.d.screen_on()
-        self.d.swipe(500, 1000, 500, 500, 400)
-        sleep(2)
+        # self.d.screen_on()
+        # sleep(0.5)
+        # self.d.swipe(500, 1300, 500, 200, duration=0.1)
+        # sleep(0.5)
         self.app_start()
 
         self.games_btn.click()
         self.main()
 
+    def stop(self):
+        self.d.app_stop(self.package)
+        sleep(3)
+        self.d.screen_off()
+
     def app_start(self):
+        self.d.app_stop(self.package)
+        sleep(0.5)
         if not self.check_app():
             self.d.app_start(self.package, self.activity)
-            while True:
+            while self.alive:
                 if self.home_btn.exists:
                     break
-                sleep(0.5)
+                sleep(2)
 
     def check_app(self):
         if self.d.app_current()['package'] != self.package:
@@ -140,14 +155,33 @@ class FarmBot:
         
         return True
 
+    def check_ads(self):
+        if self.d.app_current()['activity'] != self.ads_activity:
+            return False
+        
+        return True
+
+    def check_pm(self):
+        if self.d.app_current()['activity'] != self.pm_avtivity:
+            return False
+        
+        return True
+
+    def check_connect(self):
+        try:
+            if self.d.app_current():
+                return True
+        except:
+            return False
+        
+        return False
+
     def get_balance(self):
         pass
 
     #!!!!!!!!!!EDIT!!!!!!!!!!!!!!
     def connect_deviec(self, addres):
-        while True:
-            try:
-                self.d = u2.connect_adb_wifi(addres)
-                return
-            except:
-                sleep(10)
+        pass
+
+    def t(self):
+        return True
--- main.py
+++ main.py
@@ -1,12 +1,109 @@
 from time import sleep
 from farm_bot import FarmBot
+import streamlit as st
+import json
+from threading import Thread
 
-with open('addr.txt', 'r') as file:
-    device = file.readline()
+def update():
+    if st.session_state.conn:
+        st.session_state.data['start_disabled'] = st.session_state.start_disabled
+        st.session_state.data['stop_disabled'] = st.session_state.stop_disabled
+        st.session_state.data['ip'] =   st.session_state.ip
+        with open('data.json', 'w') as file:
+            json.dump(st.session_state.data, file, indent=4)
+
+def connect(addr):
+    if len(addr) < 6:
+        st.toast('EMPTY')
+        return
+        
+    st.toast('CONNECT..', icon='🔰')
+    st.session_state.bot = FarmBot(addr)
+    if st.session_state.bot.check_connect():
+        st.session_state.start_disabled = st.session_state.data['start_disabled']
+        st.session_state.stop_disabled = st.session_state.data['stop_disabled']
+
+        if st.session_state.stop_disabled:
+            st.session_state.start_disabled = False
+
+        st.toast('CONNECTING', icon='✅')
+    else:
+        st.toast('ERROR', icon='❌')
+
+def start():
+    try:
+        st.session_state.conn = True
+        Thread(target=st.session_state.bot.start).start()
+        st.session_state.connect_item_disabled = True
+        st.session_state.start_disabled = True
+        st.session_state.stop_disabled = False
+        st.toast('START', icon='✅')
+    except:
+        st.toast('ERROR START')
+
+def stop():
+    st.session_state.conn = True
+    st.session_state.connect_item_disabled = False
+    st.session_state.start_disabled = False
+    st.session_state.stop_disabled = True
+    st.session_state.bot.stop()
+    st.session_state.bot.alive = False
+    st.toast('STOP', icon='⛔')
 
 def main():
-    bot = FarmBot(device)
-    print('Start Farm')
-    bot.start()
 
-main()
+    with open('data.json', 'r') as file:
+        st.session_state.data = json.load(file)
+
+    if 'conn' not in st.session_state:
+        st.session_state.conn = False
+
+    if 'connect_item_disabled' not in st.session_state:
+        st.session_state.connect_item_disabled = False
+
+    if 'start_disabled' not in st.session_state:
+        st.session_state.start_disabled = True
+
+    if 'stop_disabled' not in st.session_state:
+        st.session_state.stop_disabled = True
+
+    st.markdown("""
+    <style>
+        div.stButton {
+            text-align:center;
+        }
+    </style>""", unsafe_allow_html=True)
+
+    st.markdown("""
+    <style>
+        div.stSpinner > div {
+            text-align: center;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>""", unsafe_allow_html=True)
+
+    c1 = st.container()
+    col1, col2 = c1.columns(2)
+    c2 = st.container()
+
+    with c2.expander('CONNECT DEVICE'):
+        ip = st.text_input('IP', key='ip', value=st.session_state.data['ip'], placeholder='IP', label_visibility='hidden', disabled=st.session_state.connect_item_disabled)
+        s = st.button('CONNECT', on_click=connect, args=[ip], disabled=st.session_state.connect_item_disabled)
+
+    col1.button('START', on_click=start, disabled=st.session_state.start_disabled)
+    col2.button('STOP', on_click=stop, disabled=st.session_state.stop_disabled)
+
+    update()
+
+    #e451c44d0311
+    # bot = FarmBot()
+    # print('Start Farm')
+    # try:
+    #     # bot.start()
+    #     bot.aim()
+    # except Exception as e:
+    #     print(e)
+
+if __name__ == '__main__':
+    main()
